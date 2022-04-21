ea2b3a7b07f1070933c76819c3b1fb469945b3cd
Andrzej <annddrzej@gmail.com>
2022-04-21 20:27:35 +0200

    Add POST mapping for seat purchase

--- src/main/java/com/example/cinema/CinemaController.java
+++ src/main/java/com/example/cinema/CinemaController.java
@@ -1,7 +1,12 @@
 package com.example.cinema;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +19,13 @@ public class CinemaController {
     private final List<Seat> seats = new ArrayList<>();
 
     {
-        for (int i = 0; i < totalRows; i++) {
-            for (int j = 0; j < totalColumns; j++) {
-                Seat seat = new Seat(i + 1, j + 1);
+        for (int i = 1; i <= totalRows; i++) {
+            for (int j = 1; j <= totalColumns; j++) {
+                int maxPremiumRowNo = 4;
+                int premiumPrice = 10;
+                int regularPrice = 8;
+                int price = i <= maxPremiumRowNo ? premiumPrice : regularPrice;
+                Seat seat = new Seat(i, j, price);
                 seats.add(seat);
             }
         }
@@ -27,6 +36,28 @@ public class CinemaController {
     @GetMapping("/seats")
     public CinemaSeats getCinemaRoom() {
         return cinemaSeats;
+    }
+
+    @PostMapping("/purchase")
+    public Seat purchase(@RequestBody Seat wantedSeat) {
+
+        if (cinemaSeats.getAvailableSeats().contains(wantedSeat)) {
+            int index = cinemaSeats.getAvailableSeats().indexOf(wantedSeat);
+            Seat seat = cinemaSeats.getAvailableSeats().get(index);
+
+            if (seat.isPurchased()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "The ticket has been already purchased!");
+            } else {
+                seat.setPurchased(true);
+                cinemaSeats.getAvailableSeats().set(index, seat);
+                return seat;
+            }
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "The number of a row or a column is out of bounds!");
+        }
     }
 
 }
--- src/main/java/com/example/cinema/Seat.java
+++ src/main/java/com/example/cinema/Seat.java
@@ -1,15 +1,20 @@
 package com.example.cinema;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Seat {
     private int row;
     private int column;
+    private int price;
+    private boolean isPurchased = false;
 
     public Seat() {
     }
 
-    public Seat(int row, int column) {
+    public Seat(int row, int column, int price) {
         this.row = row;
         this.column = column;
+        this.price = price;
     }
 
     public int getRow() {
@@ -26,6 +31,41 @@ public class Seat {
 
     public void setColumn(int column) {
         this.column = column;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @JsonIgnore
+    public boolean isPurchased() {
+        return isPurchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        isPurchased = purchased;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seat seat = (Seat) o;
+
+        if (row != seat.row) return false;
+        return column == seat.column;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + column;
+        return result;
     }
 
 }
